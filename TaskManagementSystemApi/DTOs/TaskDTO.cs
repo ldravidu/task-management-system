a9cec246191bcea4bf90af5eeb68f92a735a825a
Ravidu Liyanage <32922674+ldravidu@users.noreply.github.com>
2025-04-05 13:14:57 +0530

    Add TaskDTO and update Task model with required fields and nullable properties

---
+++ TaskManagementSystemApi/DTOs/TaskDTO.cs
@@ -0,0 +1,30 @@
+namespace TaskManagementSystemApi.DTOs
+{
+    public class TaskDTO
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public Models.TaskPriority Priority { get; set; } = Models.TaskPriority.Medium;
+        public Models.TaskStatus Status { get; set; } = Models.TaskStatus.Todo;
+
+        public long CreatedById { get; set; }
+        public long? AssignedToId { get; set; }
+        public long? ProjectId { get; set; }
+    }
+
+    public class CreateTaskDTO
+    {
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public Models.TaskPriority Priority { get; set; } = Models.TaskPriority.Medium;
+        public Models.TaskStatus Status { get; set; } = Models.TaskStatus.Todo;
+
+        public long? AssignedToId { get; set; }
+        public long? ProjectId { get; set; }
+    }
+}
--- TaskManagementSystemApi/Models/Task.cs
+++ TaskManagementSystemApi/Models/Task.cs
@@ -3,23 +3,23 @@ namespace TaskManagementSystemApi.Models;
 public class Task
 {
     public long Id { get; set; }
-    public string? Title { get; set; }
+    public required string Title { get; set; }
     public string? Description { get; set; }
-    public DateTime DueDate { get; set; }
+    public DateTime? DueDate { get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime UpdatedDate { get; set; }
     public TaskStatus Status { get; set; }
     public TaskPriority Priority { get; set; }
 
     public long CreatedById { get; set; }
-    public long AssignedToId { get; set; }
-    public long ProjectId { get; set; }
+    public long? AssignedToId { get; set; }
+    public long? ProjectId { get; set; }
 
 }
 
 public enum TaskStatus
 {
-    New,
+    Todo,
     InProgress,
     Completed,
     Closed
