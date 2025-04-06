b5da7c8cc6c730dcebdb060ce4e876d88e527ad4
Ravidu Liyanage <32922674+ldravidu@users.noreply.github.com>
2025-04-06 10:45:22 +0530

    Refactor ApplicationDbContext to configure relationships for Task and User models

--- TaskManagementSystemApi/Data/ApplicationDbContext.cs
+++ TaskManagementSystemApi/Data/ApplicationDbContext.cs
@@ -7,4 +7,35 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
     public DbSet<Models.Task> Tasks { get; set; }
+    public DbSet<Models.User> Users { get; set; }
+    public DbSet<Models.Project> Projects { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Models.Task>()
+            .HasOne(t => t.AssignedTo)
+            .WithMany(u => u.AssignedTasks)
+            .HasForeignKey(t => t.AssignedToId)
+            .IsRequired(false)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Models.Task>()
+            .HasOne(t => t.CreatedBy)
+            .WithMany(u => u.CreatedTasks)
+            .HasForeignKey(t => t.CreatedById)
+            .IsRequired(false)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Models.Task>()
+            .HasOne(t => t.Project)
+            .WithMany(p => p.Tasks)
+            .HasForeignKey(t => t.ProjectId)
+            .IsRequired(false)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Models.User>()
+            .HasMany(u => u.MemberOfProjects)
+            .WithMany(p => p.Members)
+            .UsingEntity(j => j.ToTable("ProjectMembers"));
+    }
 }
